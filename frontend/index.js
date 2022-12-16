16ea3f2515d4932c8429bf8f8bfac591e48e4cf3
sharon <sharonchepkirui665@gmail.com>
2022-12-16 03:43:06 +0300

    commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
-// document.addEventListener("DOMContentLoaded" ,()=>{
-//     fetchAllPatients()
-// })
-// // GET
+document.addEventListener("DOMContentLoaded" ,()=>{
+    fetchAllPatients()
+})
+// GET
 function patientsDetails(data){
 fetch("http://localhost:3000/patientDetails")
 .then(response=>response.json())
@@ -15,34 +15,34 @@ fetch("http://localhost:3000/patientDetails")
     
     })) 
 }
-// // POST
-// function displayEachPatient(data){
-//     const divBody = document.getElementById("displayPatients")
-//     const name =document.createElement('p')
-//     name.textContent=data.name
-//     const id = document.createElement('p')
-//     id.textContent=data.id
-//     const gender =document.createElement('p')
-//     gender.textContent=data.gender
-//     const age=document.createElement('p')
-//     age.textContent=data.age
-//     const residence =document.createElement('p')
-//     residence.textContent=data.residence
-//     const date =document.createElement('p')
-//     date.textContent = data.date
-//     const weight =document.createElement('p')
-//     weight.textContent = data.weight
-//     divBody.appendChild(id)
-//     divBody.appendChild(name)
-//     divBody.appendChild(gender)
-//     divBody.appendChild(age)
-//     divBody.appendChild(residence)
-//     divBody.appendChild(date)
-//     divBody.appendChild(weight) 
+// POST
+function displayEachPatient(data){
+    const divBody = document.getElementById("displayPatients")
+    const name =document.createElement('p')
+    name.textContent=data.name
+    const id = document.createElement('p')
+    id.textContent=data.id
+    const gender =document.createElement('p')
+    gender.textContent=data.gender
+    const age=document.createElement('p')
+    age.textContent=data.age
+    const residence =document.createElement('p')
+    residence.textContent=data.residence
+    const date =document.createElement('p')
+    date.textContent = data.date
+    const weight =document.createElement('p')
+    weight.textContent = data.weight
+    divBody.appendChild(id)
+    divBody.appendChild(name)
+    divBody.appendChild(gender)
+    divBody.appendChild(age)
+    divBody.appendChild(residence)
+    divBody.appendChild(date)
+    divBody.appendChild(weight) 
 
-// }
-// let form = document.querySelector("#patientForm")
-// form = document.addEventListener("submit", postingData)
+}
+let form = document.querySelector("#patientForm")
+form = document.addEventListener("submit", postingData)
 
 function postingData(event){
     event.preventDefault()
@@ -59,18 +59,18 @@ function postingData(event){
 console.log(patientDetails)
 postingDetails(patientDetails)
 }
-// function postingDetails(patientDetails){
-//     fetch("http://localhost:3000/patientDetails",{
-//     method :"POST",
-//     headers:{
-//         "Content-Type" :"application/json", 
-//         Accept:"application/json"
-//     },
-//      body:JSON.stringify(patientDetails)
-// })
-// .then(response=>response.json())
-// .then(data=>console.log(data))
-// }
+function postingDetails(patientDetails){
+    fetch("http://localhost:3000/patientDetails",{
+    method :"POST",
+    headers:{
+        "Content-Type" :"application/json", 
+        Accept:"application/json"
+    },
+     body:JSON.stringify(patientDetails)
+})
+.then(response=>response.json())
+.then(data=>console.log(data))
+}
 
 
 // // DELETE
